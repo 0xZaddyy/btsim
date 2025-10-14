1d388f8b9ed8be8092cbc6fb2a448bc4d7314d0c
Armin Sabouri <armins88@gmail.com>
2025-10-14 14:06:10 -0400

    Break up project into multiple files

---
+++ src/transaction.rs
@@ -0,0 +1,244 @@
+use crate::wallet::{AddressHandle, AddressId, WalletHandle, WalletHandleMut};
+use crate::Simulation;
+use bitcoin::transaction::{predict_weight, InputWeightPrediction};
+use bitcoin::{Amount, Weight};
+use bitcoin::{FeeRate, ScriptBuf, WitnessProgram};
+
+define_entity!(
+    Tx,
+    {
+        // version, locktime, witness flag
+        pub(crate) inputs: Vec<Input>,
+        pub(crate) outputs: Vec<Output>,
+    },
+    {
+        pub(crate) fee: Amount,
+        pub(crate) weight: Weight,
+    }
+);
+
+// TODO rename to OutputId?
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub(crate) struct Outpoint {
+    pub(crate) txid: TxId,
+    pub(crate) index: usize,
+}
+
+impl<'a> Outpoint {
+    fn with(&self, sim: &'a Simulation) -> OutputHandle<'a> {
+        OutputHandle {
+            sim,
+            outpoint: *self,
+        }
+    }
+}
+
+// TODO rename to InputData?
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub(crate) struct Input {
+    pub(crate) outpoint: Outpoint, // sequence,
+                                   // witness?
+}
+
+pub(crate) struct InputId {
+    pub(crate) txid: TxId,
+    pub(crate) index: usize,
+}
+
+pub(crate) struct InputHandle<'a> {
+    sim: &'a Simulation,
+    pub(crate) id: InputId,
+}
+
+impl<'a> InputHandle<'a> {
+    pub(crate) fn data(&self) -> &'a Input {
+        &self.sim.get_tx(self.id.txid).data().inputs[self.id.index]
+    }
+
+    pub(crate) fn prevout(&self) -> OutputHandle<'a> {
+        self.data().outpoint.with(self.sim)
+    }
+}
+
+impl<'a> From<InputHandle<'a>> for Output {
+    fn from(handle: InputHandle<'a>) -> Output {
+        *handle.prevout().data()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub(crate) struct Output {
+    pub(crate) amount: Amount,
+    pub(crate) address_id: AddressId,
+}
+
+impl From<Output> for bitcoin::transaction::TxOut {
+    fn from(o: Output) -> Self {
+        // FIXME refactor into fn encode_as_txo(enum { AddressId, Index, Outpoint  })
+        // TODO handle multiple address types
+        let mut program = [0u8; 32];
+        // TODO tag, segregate from txos encoding indexes?
+        program[0] = o
+            .address_id
+            .0
+            .try_into()
+            .expect("TODO support more than 256 addresses");
+
+        let witness_program =
+            WitnessProgram::new(bitcoin::WitnessVersion::V1, &program[..]).unwrap();
+        let script_pubkey = ScriptBuf::new_witness_program(&witness_program);
+
+        bitcoin::transaction::TxOut {
+            value: o.amount,
+            script_pubkey,
+        }
+    }
+}
+
+impl Output {
+    fn size(&self) -> usize {
+        bitcoin::transaction::TxOut::from(*self).size()
+    }
+
+    fn address<'a>(&self, sim: &'a Simulation) -> AddressHandle<'a> {
+        self.address_id.with(sim)
+    }
+
+    fn wallet<'a>(&self, sim: &'a Simulation) -> WalletHandle<'a> {
+        self.address(sim).wallet()
+    }
+
+    fn wallet_mut<'a>(&self, sim: &'a mut Simulation) -> WalletHandleMut<'a> {
+        let owner_id = self.address(sim).data().wallet_id;
+        owner_id.with_mut(sim)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub(crate) struct OutputHandle<'a> {
+    sim: &'a Simulation,
+    pub(crate) outpoint: Outpoint,
+}
+
+impl From<OutputHandle<'_>> for InputWeightPrediction {
+    fn from(output: OutputHandle<'_>) -> Self {
+        Self::from(output.address())
+    }
+}
+
+impl<'a> OutputHandle<'a> {
+    pub(crate) fn new(sim: &'a Simulation, outpoint: Outpoint) -> Self {
+        Self { sim, outpoint }
+    }
+
+    pub(crate) fn outpoint(&self) -> Outpoint {
+        self.outpoint
+    }
+
+    pub(crate) fn data(&self) -> &'a Output {
+        &self.sim.get_tx(self.outpoint.txid).data().outputs[self.outpoint.index]
+    }
+
+    pub(crate) fn address(&'a self) -> AddressHandle<'a> {
+        self.data().address_id.with(self.sim)
+    }
+
+    pub(crate) fn wallet(&'a self) -> WalletHandle<'a> {
+        self.data().wallet(self.sim)
+    }
+
+    pub(crate) fn wallet_mut<'b>(&self, sim: &'b mut Simulation) -> WalletHandleMut<'b> {
+        self.data().wallet_mut(sim)
+    }
+}
+
+impl<'a> From<OutputHandle<'a>> for Output {
+    fn from(handle: OutputHandle<'a>) -> Output {
+        *handle.data()
+    }
+}
+
+impl<'a> From<OutputHandle<'a>> for Outpoint {
+    fn from(handle: OutputHandle<'a>) -> Outpoint {
+        handle.outpoint
+    }
+}
+
+impl<'a> TxHandle<'a> {
+    pub(crate) fn data(&self) -> &'a TxData {
+        &self.sim.tx_data[self.id.0]
+    }
+
+    pub(crate) fn info(&self) -> &'a TxInfo {
+        &self.sim.tx_info[self.id.0]
+    }
+
+    pub(crate) fn is_coinbase(&self) -> bool {
+        self.data().inputs.is_empty()
+    }
+
+    pub(crate) fn outpoints(&self) -> impl Iterator<Item = Outpoint> {
+        let txid = self.id;
+        (0..self.data().outputs.len()).map(move |index| Outpoint { txid, index })
+    }
+    pub(crate) fn outputs(&'a self) -> impl Iterator<Item = OutputHandle<'a>> {
+        self.outpoints().map(|outpoint| OutputHandle {
+            sim: self.sim,
+            outpoint,
+        })
+    }
+
+    pub(crate) fn inputs(&'a self) -> impl Iterator<Item = InputHandle<'a>> {
+        let txid = self.id;
+        let sim = self.sim;
+        (0..self.data().inputs.len()).map(move |index| InputHandle {
+            sim,
+            id: InputId { txid, index },
+        })
+    }
+
+    // TODO fn prevouts(self) -> impl IntoIterator??
+    // TODO confirmed
+    // TODO previous txs
+}
+
+impl Default for TxData {
+    fn default() -> Self {
+        Self {
+            inputs: Vec::default(),
+            outputs: Vec::default(),
+        }
+    }
+}
+
+impl TxInfo {
+    pub(crate) fn new(tx: &TxData, sim: &Simulation) -> Self {
+        // TODO Result with invalid txn error?
+        // TODO refactor into a method.. on Simulation? on tx accepting simulation?
+        let prevouts = tx.inputs.iter().map(|i| i.outpoint.with(&sim));
+
+        let weight = predict_weight(
+            prevouts.clone().map(|i| InputWeightPrediction::from(i)),
+            tx.outputs.iter().map(|o| o.size()),
+        );
+
+        // TODO separate to a different index struct
+        let total_input_amount: Amount = prevouts.map(|o| o.data().amount).sum();
+        let total_output_amount = tx.outputs.iter().map(|o| o.amount).sum();
+
+        // TODO Result
+        assert!(tx.inputs.is_empty() || total_output_amount <= total_input_amount);
+
+        let fees = if tx.inputs.is_empty() {
+            Amount::default()
+        } else {
+            total_input_amount - total_output_amount // TODO
+        };
+
+        TxInfo { fee: fees, weight }
+    }
+
+    fn feerate(self) -> FeeRate {
+        self.fee / self.weight
+    }
+}
---
+++ src/wallet.rs
@@ -0,0 +1,213 @@
+use crate::{
+    blocks::{BroadcastSetHandleMut, BroadcastSetId},
+    Simulation,
+};
+use bdk_coin_select::{
+    metrics::LowestFee, Candidate, ChangePolicy, CoinSelector, Drain, DrainWeights, Target,
+    TR_DUST_RELAY_MIN_VALUE, TR_KEYSPEND_TXIN_WEIGHT,
+};
+use bitcoin::{transaction::InputWeightPrediction, Amount};
+use im::{OrdSet, Vector};
+
+use crate::transaction::*;
+
+define_entity_mut_updatable!(
+    Wallet,
+    {
+        pub(crate) id: WalletId,
+        pub(crate) addresses: Vec<AddressId>,         // TODO split into internal/external?
+        pub(crate) own_transactions: Vec<TxId>,       // transactions originating from this wallet
+        pub(crate) last_wallet_info_id: WalletInfoId, // Monotone
+    },
+    {
+        pub(crate) broadcast_set_id: BroadcastSetId,
+        pub(crate) payment_obligations: OrdSet<PaymentObligationId>,
+        pub(crate) expected_payments: OrdSet<PaymentObligationId>,
+        pub(crate) broadcast_transactions: Vector<TxId>,
+        pub(crate) received_transactions: Vector<TxId>,
+        pub(crate) confirmed_utxos: OrdSet<Outpoint>,    // TODO locktimes
+        pub(crate) unconfirmed_transactions: OrdSet<TxId>,
+        pub(crate) unconfirmed_txos: OrdSet<Outpoint>,  // compute CPFP cost
+        pub(crate) unconfirmed_spends: OrdSet<Outpoint>, // RBFable
+    }
+);
+
+// TODO WalletHandle
+impl<'a> WalletHandle<'a> {
+    pub(crate) fn data(&self) -> &'a WalletData {
+        &self.sim.wallet_data[self.id.0]
+    }
+
+    pub(crate) fn info(&self) -> &'a WalletInfo {
+        &self.sim.wallet_info[self.data().last_wallet_info_id.0]
+    }
+
+    // TODO give utxo list as argument so that different variants can be used
+    // TODO return change information
+    pub(crate) fn select_coins(
+        &self,
+        target: Target,
+        long_term_feerate: bitcoin::FeeRate,
+    ) -> (impl Iterator<Item = OutputHandle<'a>>, Drain) {
+        // TODO change
+        // TODO group by address
+        let utxos: Vec<OutputHandle<'a>> = self.unspent_coins().collect();
+
+        let candidates: Vec<Candidate> = utxos
+            .iter()
+            .enumerate()
+            .map(|(i, o)| Candidate {
+                value: o.data().amount.to_sat(),
+                weight: TR_KEYSPEND_TXIN_WEIGHT,
+                input_count: 1,
+                is_segwit: true,
+            })
+            .collect();
+
+        let mut coin_selector = CoinSelector::new(&candidates);
+        let drain_weights = DrainWeights::default();
+
+        let dust_limit = TR_DUST_RELAY_MIN_VALUE;
+
+        let long_term_feerate = bdk_coin_select::FeeRate::from_sat_per_wu(
+            long_term_feerate.to_sat_per_kwu() as f32 * 1e-3,
+        );
+
+        let change_policy = ChangePolicy::min_value_and_waste(
+            drain_weights,
+            dust_limit,
+            target.fee.rate,
+            long_term_feerate,
+        );
+
+        let metric = LowestFee {
+            target,
+            long_term_feerate,
+            change_policy,
+        };
+
+        match coin_selector.run_bnb(metric, 100_000) {
+            Err(err) => {
+                println!("BNB failed to find a solution: {}", err);
+
+                coin_selector
+                    .select_until_target_met(target)
+                    .expect("coin selection should always succeed since payments consider budger lower bound");
+            }
+            Ok(score) => {
+                println!("BNB found a solution with score {}", score);
+            }
+        };
+
+        let selection = coin_selector
+            .apply_selection(&utxos)
+            .cloned()
+            .collect::<Vec<_>>();
+
+        let change = coin_selector.drain(target, change_policy);
+
+        (selection.into_iter(), change)
+    }
+
+    fn potentially_spendable_txos(&self) -> impl Iterator<Item = OutputHandle<'a>> + '_ {
+        self.info()
+            .confirmed_utxos
+            .iter()
+            .chain(self.info().unconfirmed_txos.iter())
+            .map(|outpoint| OutputHandle::new(self.sim, *outpoint))
+    }
+
+    fn unspent_coins(&self) -> impl Iterator<Item = OutputHandle<'a>> + '_ {
+        self.potentially_spendable_txos()
+            .filter(|o| !self.info().unconfirmed_spends.contains(&o.outpoint()))
+    }
+
+    fn double_spendable_coins(&self) -> impl Iterator<Item = OutputHandle<'a>> + '_ {
+        self.potentially_spendable_txos()
+            .filter(|o| self.info().unconfirmed_spends.contains(&o.outpoint()))
+    }
+}
+
+impl<'a> WalletHandleMut<'a> {
+    pub(crate) fn data_mut<'b>(&'b mut self) -> &'b mut WalletData {
+        &mut self.sim.wallet_data[self.id.0]
+    }
+
+    pub(crate) fn new_address(&mut self) -> AddressId {
+        let id = self.sim.new_address(self.id);
+        self.sim.wallet_data[self.id.0].addresses.push(id);
+        id
+    }
+
+    pub(crate) fn new_tx<F>(&mut self, build: F) -> TxHandle
+    where
+        F: FnOnce(&mut TxData, &Simulation),
+    {
+        let id = self.sim.new_tx(build);
+        self.data_mut().own_transactions.push(id);
+        TxHandle::new(self.sim, id)
+    }
+
+    pub(crate) fn broadcast(
+        &'a mut self,
+        txs: impl IntoIterator<Item = TxId>,
+    ) -> BroadcastSetHandleMut<'a> {
+        let mut wallet_info = self.info().clone();
+
+        let txs = Vector::from_iter(txs);
+
+        wallet_info.broadcast_transactions.append(txs.clone());
+
+        // TODO refactor boilerplate for updating wallet ID
+        let id = WalletInfoId(self.sim.wallet_info.len());
+        self.sim.wallet_info.push(wallet_info);
+        let data = self.data_mut();
+        data.last_wallet_info_id = id;
+
+        self.sim.broadcast(txs)
+    }
+}
+define_entity!(
+    PaymentObligation,
+    {
+        pub(crate) deadline: usize, // block height? time step?
+        pub(crate) amount: Amount,
+        pub(crate) from: WalletId,
+        pub(crate) to: WalletId,
+    },
+    {
+        // TODO coin selection strategy agnostic (pessimal?) spendable balance lower
+        // bound
+    }
+);
+
+define_entity!(Address, {
+    pub(crate) wallet_id: WalletId,
+    // TODO script_type
+    // TODO internal
+    // TODO silent payments
+}, {});
+
+impl From<AddressData> for InputWeightPrediction {
+    fn from(_: AddressData) -> Self {
+        // TODO match on script_type
+        bitcoin::transaction::InputWeightPrediction::P2TR_KEY_DEFAULT_SIGHASH
+    }
+}
+
+impl From<AddressHandle<'_>> for InputWeightPrediction {
+    fn from(address: AddressHandle<'_>) -> Self {
+        Self::from(address.data().clone())
+    }
+}
+
+// TODO traits?
+impl<'a> AddressHandle<'a> {
+    pub(crate) fn data(&'a self) -> &'a AddressData {
+        &self.sim.address_data[self.id.0]
+    }
+
+    pub(crate) fn wallet(&self) -> WalletHandle<'a> {
+        self.data().wallet_id.with(self.sim)
+    }
+}
